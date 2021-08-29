e57b3b777aa0d048752693bc03481d405fcb1be1
alinemartinsgh <aline.p.martins@globahitss.com.br>
2021-08-29 12:33:00 -0300

    add Navigation e assets

---
+++ src/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './view/Home';
+
+const Stack = createNativeStackNavigator()
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerTitleAlign: 'center'
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
